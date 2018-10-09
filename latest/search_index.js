7f0c2e6d1427ed7514816c25310a3ff4a0465511
autodocs <autodocs>
2018-10-09 19:37:33 +0000

    build based on 8d5723f

---
+++ latest/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "PerformanceIndex",
+    "title": "PerformanceIndex",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#PerformanceIndex-1",
+    "page": "PerformanceIndex",
+    "title": "PerformanceIndex",
+    "category": "section",
+    "text": "Documentation goes here."
+},
+
+]}
