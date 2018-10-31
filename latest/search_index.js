c19cdae97662736198080ff9a8aa3cd7a6884588
autodocs <autodocs>
2018-10-31 22:23:05 +0000

    build based on fb537c5

--- latest/search_index.js
+++ latest/search_index.js
@@ -2,18 +2,98 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "index.html#",
-    "page": "PerformanceIndex",
-    "title": "PerformanceIndex",
+    "page": "Home",
+    "title": "Home",
     "category": "page",
     "text": ""
 },
 
 {
     "location": "index.html#PerformanceIndex-1",
-    "page": "PerformanceIndex",
+    "page": "Home",
     "title": "PerformanceIndex",
     "category": "section",
-    "text": "Documentation goes here."
+    "text": "PerformanceIndex provides Julia tools for ..."
+},
+
+{
+    "location": "details.html#",
+    "page": "Details",
+    "title": "Details",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "details.html#Details-1",
+    "page": "Details",
+    "title": "Details",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "details.html#PerformanceIndex.IAE.iae",
+    "page": "Details",
+    "title": "PerformanceIndex.IAE.iae",
+    "category": "function",
+    "text": "```julia\niae(error)\n```\nIAE (Integral Absolute Error) from error vector.\nInputs:\nerror -> error vector.\n\n\n\n\n\n```julia\niae(error)\n```\nIAE (Integral Absolute Error) from vector of error vector.\nInputs:\nerror -> vector of error vector.\n\n\n\n\n\n```julia\niae(x, t, desired)\n```\nIAE (Integral Absolute Error) from error betwwen desired value and controler output.\nInputs:\nx -> controler output vector; \nt -> time vector;\ndesired -> desired value function\n\n.\n\n\n\n\n\n"
+},
+
+{
+    "location": "details.html#IAE-1",
+    "page": "Details",
+    "title": "IAE",
+    "category": "section",
+    "text": "iae"
+},
+
+{
+    "location": "details.html#PerformanceIndex.ISE.ise",
+    "page": "Details",
+    "title": "PerformanceIndex.ISE.ise",
+    "category": "function",
+    "text": "```julia\nise(error)\n```\nIntegral Squared Error (ISE) fromm error vector.\nInputs:\nerror -> error vector.\n\n\n\n\n\n```julia\nise(error)\n```\nIntegral Squared Error (ISE) from vector of error vector.\nInputs:\nerror -> vector of error vector.\n\n\n\n\n\n```julia\nise(x, t, desired)\n```\nIntegral Squared Error (ISE) from error betwwen desired value and controler output.\nInputs:\nx -> controler output vector;\nt -> time vector;\ndesired -> desired value function.\n\n\n\n\n\n"
+},
+
+{
+    "location": "details.html#ISE-1",
+    "page": "Details",
+    "title": "ISE",
+    "category": "section",
+    "text": "ise"
+},
+
+{
+    "location": "details.html#PerformanceIndex.ITAE.itae",
+    "page": "Details",
+    "title": "PerformanceIndex.ITAE.itae",
+    "category": "function",
+    "text": "```julia\nitae(error, time)\n```\nITAE (Integral of Time-Weighted Absolute Error) from error vector.\nInputs:\nerror -> error vector.\ntime  -> time vector.\n\n\n\n\n\n```julia\nitae(error, time)\n``` \nITAE (Integral of Time-Weighted Absolute Error) from vector of error vector.\nInputs:\nerror -> vector of error vector.\ntime  -> time vector.\n\n\n\n\n\n```julia\nitae(x, t, desired)\n```\nITAE (Integral of Time-Weighted Absolute Error) from error betwwen desired value and controler output.\nInputs:\nx -> controler output vector.\nt -> time vector.\ndesired -> desired value function.\n\n\n\n\n\n"
+},
+
+{
+    "location": "details.html#ITAE-1",
+    "page": "Details",
+    "title": "ITAE",
+    "category": "section",
+    "text": "itae"
+},
+
+{
+    "location": "details.html#PerformanceIndex.ITSE.itse",
+    "page": "Details",
+    "title": "PerformanceIndex.ITSE.itse",
+    "category": "function",
+    "text": "```julia\nitse(error, time)\n```\nIntegral of Time-Weighted Squared Error (ITSE) fromm error vector.\nInputs:\nerror -> error vector.\ntime  -> time vector.\n\n\n\n\n\n```julia\nitse(error, time)\n```\nIntegral of Time-Weighted Squared Error (ITSE) from vector of error vector.\nInputs:\nerror -> vector of error vector.\ntime  -> time vector.\n\n\n\n\n\n```julia\nitse(x, t, desired)\n```\nIntegral of Time-Weighted Squared Error (ITSE) from error betwwen desired value and controler output.\nInputs:\nx -> controler output vector;\nt -> time vector;\ndesired -> desired value function.\n\n\n\n\n\n"
+},
+
+{
+    "location": "details.html#ITSE-1",
+    "page": "Details",
+    "title": "ITSE",
+    "category": "section",
+    "text": "itse"
 },
 
 ]}
