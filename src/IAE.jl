321a58e2f40458834e99876edfd2788ce85c4fd7
Phelipe Wesley <phelipewesleydeoliveira@gmail.com>
2018-10-31 18:56:57 -0300

    update code

---
+++ src/IAE.jl
@@ -0,0 +1,46 @@
+module IAE
+
+export iae
+
+"""
+    ```julia
+    iae(error)
+    ```
+    IAE (Integral Absolute Error) from error vector.
+    Inputs:
+    error -> error vector.
+"""
+function iae(error::Vector{T}) where T
+    sum(abs.(error))
+end    
+
+"""
+    ```julia
+    iae(error)
+    ```
+    IAE (Integral Absolute Error) from vector of error vector.
+    Inputs:
+    error -> vector of error vector.
+"""
+function iae(error::Vector{Vector{T}}) where T
+    map(x->sum(abs.(x)), error)
+end    
+
+
+""" 
+    ```julia
+    iae(x, t, desired)
+    ```
+    IAE (Integral Absolute Error) from error betwwen desired value and controler output.
+    Inputs:
+    x -> controler output vector; 
+    t -> time vector;
+    desired -> desired value function
+   .
+"""
+function iae(x::Vector{T}, t::Vector{T}, desired::Function) where T
+    x = x .- map(desired, t)
+    sum(abs.(x))
+end    
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -15,3 +15,11 @@ end
 @test ise(error) == 14.
 @test prod(ise([error, error]) .== [14., 14.])
 @test ise(data, time, desired) == 5.
+
+@test itae(error, time) == 14.
+@test prod(itae([error, error], time) .== [14., 14.])
+@test itae(data, time, desired) == 8.
+
+@test itse(error, time) == 36.
+@test prod(itse([error, error], time) .== [36., 36.])
+@test itse(data, time, desired) == 14.
